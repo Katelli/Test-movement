708b6763d94b49cf439485a9485b751760ff8ca4
Katelli <katrine.ask96@gmail.com>
2024-01-24 17:46:07 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+window.addEventListener("keydown", (event) => {
+      
+
+      let ball = document.getElementById("ball")
+      let compStyle = window.getComputedStyle(ball)
+      let ballTop = compStyle.getPropertyValue("top")
+      let ballLeft = compStyle.getPropertyValue("left")
+      
+
+    switch (event.key) {
+        case "ArrowRight":
+            if (ball.style.left >"1150"){ 
+                ball.style.left = `${parseInt(ballLeft) + 50}px`
+            }
+            break;
+
+        case "ArrowLeft":
+            if (ball.style.left >"1"){ 
+                ball.style.left = `${parseInt(ballLeft) - 50}px`
+            }
+            break;
+
+        case "ArrowDown":
+            if (ball.style.top <"650"){
+            ball.style.top = `${parseInt(ballTop) + 50}px`
+            }
+            break;
+
+        case "ArrowUp":
+            if (ball.style.top >"0"){
+            ball.style.top = `${parseInt(ballTop) - 50}px`
+            }
+            break;
+    }
+  });
