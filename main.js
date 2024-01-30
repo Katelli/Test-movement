b97625b0377c15ab652d30df048f78cb9b321b07
Katelli <katrine.ask96@gmail.com>
2024-01-30 18:52:28 +0100

    commit

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 window.addEventListener("keydown", (event) => {
       
 
-      let ball = document.getElementById("ball")
-      let compStyleB = window.getComputedStyle(ball)
-      let ballTop = compStyleB.getPropertyValue("top")
-      let ballLeft = compStyleB.getPropertyValue("left")
-      let obst = document.getElementById("obst")
-      let compStyleO = window.getComputedStyle(obst)
-      let obstTop = compStyleO.getPropertyValue("top")
-      let obstLeft = compStyleO.getPropertyValue("left")
+    let ball = document.getElementById("ball")
+    let compStyleB = window.getComputedStyle(ball)
+    let ballTop = compStyleB.getPropertyValue("top")
+    let ballLeft = compStyleB.getPropertyValue("left")
+    let obst = document.getElementById("obst")
+    let compStyleO = window.getComputedStyle(obst)
+    let obstTop = compStyleO.getPropertyValue("top")
+    let obstLeft = compStyleO.getPropertyValue("left")
       
 
     switch (event.key) {
@@ -29,12 +29,13 @@ window.addEventListener("keydown", (event) => {
             break;
 
         case "ArrowDown":
-            if (`${Number.parseInt(ballLeft) - Number.parseInt(obstLeft)}px` < "10px" && 
-            `${Number.parseInt(obstLeft) - Number.parseInt(ballLeft)}px` >= "50px" &&
-            `${Number.parseInt(ballTop) - Number.parseInt(obstTop)}px` >= "0px" && 
-            `${Number.parseInt(ballTop) - Number.parseInt(obstTop)}px` <= "100px"){
-                ball.style.left = `${Number.parseInt(ballLeft) + 0}px`
-            } else if (ball.style.top <"650"){
+            if (ball.style.top === "650px" ||
+            `${Number.parseInt(ballLeft) - Number.parseInt(obstLeft)}px` < "10" &&
+            `${Number.parseInt(obstLeft) - Number.parseInt(ballLeft)}px` < "50" &&
+            `${Number.parseInt(ballTop) - Number.parseInt(obstTop)}px` > "0" &&
+            `${Number.parseInt(ballTop) - Number.parseInt(obstTop)}px` < "100"){
+                ball.style.top = `${Number.parseInt(ballTop) + 0}px`
+            } else{
                 ball.style.top = `${Number.parseInt(ballTop) + 10}px`
             }
             break;
